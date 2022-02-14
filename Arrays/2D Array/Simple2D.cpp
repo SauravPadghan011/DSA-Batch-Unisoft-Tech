81782c3ff2c2b9264bb66d58eb6ed19b884eb28b
Saurav Padghan <sauravpadghan011@gmail.com>
2022-02-14 16:27:37 +0530

    2-D Array updated

--- Arrays/2D Array/Simple2D.cpp
+++ Arrays/2D Array/Simple2D.cpp
@@ -3,22 +3,25 @@ using namespace std;
 
 int main() {
 
-    // int m=3, n=3;
-    int arr[][3] = {1,2,3,4,5,6,7,8,9};
-    // int arr[3][3] = {1,2,3,4,5,6,7,8,9};
-    // int arr[m][n] = {
-    //                     {1,2,3},
-    //                     {4,5,6},
-    //                     {7,8,9}
-    //                 };
+    int m=3, n=3;
+    int arr[m][n];
 
-    // cout << "Enter Elements: " <<endl;
-    // for(int i=0; i<m; i++) {
-    //     for(int j=0; j<n; j++) {
-    //         cin >> arr[i][j];
-    //     }
-    // }
+    // int arr[3][3] = {1,2,3,4,5,6,7,8,9};          //**Valid
+    // int arr[][3] = {1,2,3,4,5,6,7,8,9};           //**Valid
+    // int arr[3][3] = {{1,2,3}, {4,5,6}, {7,8,9} }; //**Valid
 
+    // int arr[3][] = {1,2,3,4,5,6,7,8,9};           //**Invalid
+
+
+    cout << "Enter Elements: " <<endl;
+    for(int i=0; i<m; i++) {
+        for(int j=0; j<n; j++) {
+            cout << "arr[" << i << "][" << j << "]: ";
+            cin >> arr[i][j];
+        }
+    }
+
+    cout << "\nArray Elements: \n";
     for(int i=0; i<3; i++) {
         for(int j=0; j<3; j++) {
             cout << arr[i][j] << " ";
