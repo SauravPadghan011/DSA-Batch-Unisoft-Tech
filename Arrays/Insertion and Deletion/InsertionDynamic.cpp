4daa6e115d947cdc19b5abdd80261b25259637d7
Saurav Padghan <sauravpadghan011@gmail.com>
2022-02-14 11:01:56 +0530

    updated

--- Arrays/Insertion and Deletion/InsertionDynamic.cpp
+++ Arrays/Insertion and Deletion/InsertionDynamic.cpp
@@ -40,7 +40,8 @@ int main() {
     int element;
     cout << "Enter Element: ";
     cin >> element;
-
+    
+    
     // Creating new array with size 1 more than original
     int newSize = size+1;
     int *arr2 = new int[newSize];
