02775fe113ec88efcfd5485af46284971d2b88bc
Saurav Padghan <sauravpadghan011@gmail.com>
2022-03-16 10:08:17 +0530

    Queue Implementation

---
+++ Queue/queue.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+int front = 0;
+int last = -1;
+int arr[100];
+
+void enqueue(int val) {
+    if(last == 99) {
+        cout << "Queue is Full!";
+        return;
+    }
+    arr[++last] = val;
+}
+
+void dequeue() {
+    if(last == -1 || front > last) {
+        cout << "Queue is Empty!";
+        return;
+    }
+    int temp = arr[front];
+    front++;
+}
+
+void display() {
+    int i=front;
+    while(i < last+1) {
+        cout << arr[i] << " ";
+        i++;
+    }
+}
+
+
+int main() {
+
+    enqueue(10);
+    enqueue(20);
+    enqueue(30);
+    enqueue(40);
+    enqueue(50);
+    enqueue(60);
+
+   
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();
+    dequeue();dequeue();dequeue();
+
+    display();
+
+    return 0;
+}
